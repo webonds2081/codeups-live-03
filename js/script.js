e596f5f45486699db08ceebfacf221842a56b6e3
webonds2081 <webonds2081@gmail.com>
2022-02-18 22:28:39 +0900

    0218

--- js/script.js
+++ js/script.js
@@ -24,27 +24,30 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
   });
 
   //ドロワーメニュー
-  $("#MenuButton").click(function () {
-    // $(".l-drawer-menu").toggleClass("is-show");
-    // $(".p-drawer-menu").toggleClass("is-show");
-    $(".js-drawer-open").toggleClass("open");
-    $(".drawer-menu").toggleClass("open");
-    $("html").toggleClass("is-fixed");
-
+  $(".js-hamburger").click(function () {
+    if ($('.js-hamburger').hasClass('is-active')) {
+      $('.js-hamburger').removeClass("is-active");
+      // $("html").toggleClass("is-fixed");
+      $(".js-sp-nav").fadeOut(300);
+    } else {
+      $('.js-hamburger').addClass("is-active");
+      // $("html").toggleClass("is-fixed");
+      $(".js-sp-nav").fadeIn(300);
+    }
   });
 
 
 
-  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
+    // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
 
-  $(document).on('click', 'a[href*="#"]', function () {
-    let time = 400;
-    let header = $('header').innerHeight();
-    let target = $(this.hash);
-    if (!target.length) return;
-    let targetY = target.offset().top - header;
-    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
-    return false;
+    $(document).on('click', 'a[href*="#"]', function () {
+      let time = 400;
+      let header = $('header').innerHeight();
+      let target = $(this.hash);
+      if (!target.length) return;
+      let targetY = target.offset().top - header;
+      $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+      return false;
+    });
+
   });
-
-});
