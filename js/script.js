be0f7ba4df44313fda018509f7738d099aaf26bf
HIROKI MATSU <webonds2081@gmail.com>
2022-04-21 21:49:42 +0900

    0421

--- js/script.js
+++ js/script.js
@@ -38,16 +38,26 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
 
 
-    // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
+  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
 
-    $(document).on('click', 'a[href*="#"]', function () {
-      let time = 400;
-      let header = $('header').innerHeight();
-      let target = $(this.hash);
-      if (!target.length) return;
-      let targetY = target.offset().top - header;
-      $('html,body').animate({ scrollTop: targetY }, time, 'swing');
-      return false;
-    });
-
+  $(document).on('click', 'a[href*="#"]', function () {
+    let time = 400;
+    let header = $('header').innerHeight();
+    let target = $(this.hash);
+    if (!target.length) return;
+    let targetY = target.offset().top - header;
+    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    return false;
   });
+
+  var swiper = new Swiper(".js-works-swiper", {
+    pagination: {
+      el: ".js-works-pagination",
+    },
+    loop: true,
+    clickable: true,
+    // autoplay: {
+    //   delay: 3000,
+    // },
+  });
+});
